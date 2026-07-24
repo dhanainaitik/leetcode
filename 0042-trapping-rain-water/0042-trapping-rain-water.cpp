1c07a01a65cc3926bfc56d2cfae973950b7d940d
Naitik Dhanai <dhanainaitik585@gmail.com>
2026-07-24 17:50:34 +0530

    Time: 0 ms (100%), Space: 26 MB (57.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,37 +3,67 @@ public:
 
   
     int trap(vector<int>& height) {
-        int total = 0;
+        // int total = 0;
 
-        int n = height.size();
+        // int n = height.size();
 
-        vector<int>prefixmax(n);
+        // vector<int>prefixmax(n);
 
-        prefixmax[0] = height[0];
+        // prefixmax[0] = height[0];
 
-        for(int i=1 ; i<n ; i++){
-            prefixmax[i] = max(prefixmax[i-1],height[i]);
-            }
+        // for(int i=1 ; i<n ; i++){
+        //     prefixmax[i] = max(prefixmax[i-1],height[i]);
+        //     }
 
-        vector<int>suffixmax(n);
+        // vector<int>suffixmax(n);
 
-        suffixmax[n-1] = height[n-1];
+        // suffixmax[n-1] = height[n-1];
 
-        for(int i = n-2 ; i>0 ; i--){
-            suffixmax[i] = max(suffixmax[i+1] , height[i]);
-             }
+        // for(int i = n-2 ; i>0 ; i--){
+        //     suffixmax[i] = max(suffixmax[i+1] , height[i]);
+        //      }
         
 
 
-        for(int i=0 ; i<n ; i++){
-            int leftmax = prefixmax[i];
-           int rightmax = suffixmax[i];
-            if(height[i]<leftmax && height[i]<rightmax){
-                total += min(leftmax,rightmax) - height[i];
+        // for(int i=0 ; i<n ; i++){
+        //     int leftmax = prefixmax[i];
+        //    int rightmax = suffixmax[i];
+        //     if(height[i]<leftmax && height[i]<rightmax){
+        //         total += min(leftmax,rightmax) - height[i];
 
+        //     }
+        // }
+
+        // return total;
+
+        //optimal .... 
+
+        int n = height.size();
+        int leftMax = 0;
+        int rightMax = 0;
+        int l = 0;
+        int r = n-1;
+        int total = 0;
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(height[l]<leftMax){
+                    total += leftMax - height[l];
+                }
+                else{
+                    leftMax = height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r]<rightMax){
+                    total += rightMax - height[r];
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r--;
             }
         }
-
-        return total;
+  return total ;
     }
 };
