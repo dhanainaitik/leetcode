e0b833a84e28b05037b5501f4cfd8e8848426940
Naitik Dhanai <dhanainaitik585@gmail.com>
2026-08-02 14:16:49 +0530

    Time: 0 ms (100%), Space: 8 MB (82.86%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool check(char ch){
+
+        if(ch>='a' && ch<='z'){
+            return true;
+        }
+        else if (ch>='A' && ch <='Z'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    string reverseOnlyLetters(string s) {
+        int i = 0 ;
+        int j = s.size();
+
+        while(i<j){
+            if(check(s[i]) == true && check(s[j]) == true){
+                swap(s[i] , s[j]);
+                i++;
+                j--;
+            }
+            else if(check(s[i]) == true && check(s[j]) == false){
+                j--;
+            }
+            else if(check(s[i]) == false && check(s[j]) == true){
+                i++;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
