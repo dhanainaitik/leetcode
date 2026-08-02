4fd36850ad19b056047b86665e68af309613cf14
Naitik Dhanai <dhanainaitik585@gmail.com>
2026-08-02 16:05:04 +0530

    Time: 0 ms (100%), Space: 13.1 MB (61.89%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,30 +1,67 @@
+// class Solution {
+// public:
+//     long long pickGifts(vector<int>& gifts, int k) {
+//        while(k>0){
+//         int n = gifts.size();
+//         int maxi = gifts[0];
+//         for(int i=1 ; i<n ; i++){
+//             maxi = max(maxi,gifts[i]);
+//         }
+//         int ans = maxi - (maxi - floor(sqrt(maxi)));
+
+//         for(int i=0 ; i<n ; i++){
+//             if(gifts[i] == maxi){
+//                 gifts[i] = ans;
+//                 break; // break is added to make sure only one pile gets effected if in case there are two 100 in piles ...
+//             }
+//         }
+
+//       k--;
+//        }
+//        long long sum = 0;
+//        for(int i=0 ; i<gifts.size() ; i++){
+//            sum += gifts[i];
+//        } 
+
+//        return sum;
+
+
+//     }
+// };
+
 class Solution {
 public:
-    long long pickGifts(vector<int>& gifts, int k) {
-       while(k>0){
-        int n = gifts.size();
-        int maxi = gifts[0];
-        for(int i=1 ; i<n ; i++){
-            maxi = max(maxi,gifts[i]);
-        }
-        int ans = maxi - (maxi - floor(sqrt(maxi)));
+    long long pickGifts(vector<int>& g, int k) {
 
-        for(int i=0 ; i<n ; i++){
-            if(gifts[i] == maxi){
-                gifts[i] = ans;
-                break; // break is added to make sure only one pile gets effected if in case there are two 100 in piles ...
-            }
+        priority_queue<int>pq;
+
+        long long sum=0;
+
+        for(int i=0;i<g.size();i++){
+
+            pq.push(g[i]);
         }
 
-      k--;
-       }
-       long long sum = 0;
-       for(int i=0 ; i<gifts.size() ; i++){
-           sum += gifts[i];
-       } 
+        while(k>0){
 
-       return sum;
+            int temp=floor(sqrt(pq.top()));
 
+            pq.pop();
 
+            pq.push(temp);
+
+            k--;
+            
+     }
+
+     while(!pq.empty()){
+
+        sum=sum+pq.top();
+
+        pq.pop();
+     }
+
+     return sum;
+        
     }
 };
