6bdaec4e15450f4cbae1ebce4b28a0bc5039a056
Naitik Dhanai <dhanainaitik585@gmail.com>
2026-08-02 13:47:45 +0530

    Time: 2 ms (93.01%), Space: 22.9 MB (20.04%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+
+    vector<int> getNSR(vector<int> &height) {
+        stack<int> st;
+        int n = height.size();
+        vector<int> NSR(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && height[st.top()] >= height[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                NSR[i] = n;
+            else
+                NSR[i] = st.top();
+
+            st.push(i);
+        }
+
+        return NSR;
+    }
+
+    vector<int> getNSL(vector<int> &height) {
+        stack<int> st;
+        int n = height.size();
+        vector<int> NSL(n);
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && height[st.top()] >= height[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                NSL[i] = -1;
+            else
+                NSL[i] = st.top();
+
+            st.push(i);
+        }
+
+        return NSL;
+    }
+
+    int findMaxArea(vector<int> &height) {
+
+        vector<int> NSR = getNSR(height);
+        vector<int> NSL = getNSL(height);
+
+        int n = height.size();
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int width = NSR[i] - NSL[i] - 1;
+            int area = width * height[i];
+            maxArea = max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+        if (matrix.empty())
+            return 0;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> height(n);
+
+      
+        for (int i = 0; i < n; i++) {
+            height[i] = (matrix[0][i] == '1') ? 1 : 0;
+        }
+
+        int maxArea = findMaxArea(height);
+
+       
+        for (int row = 1; row < m; row++) {
+
+            for (int col = 0; col < n; col++) {
+
+                if (matrix[row][col] == '0')
+                    height[col] = 0;
+                else
+                    height[col]++;
+            }
+
+            maxArea = max(maxArea, findMaxArea(height));
+        }
+
+        return maxArea;
+    }
+};
