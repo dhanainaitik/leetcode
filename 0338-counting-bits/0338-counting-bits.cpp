2e64173ba0cc1ad1dd3431362541d22c7db1a086
Naitik Dhanai <dhanainaitik585@gmail.com>
2026-08-09 14:03:52 +0530

    Time: 4 ms (10.27%), Space: 11.5 MB (8.82%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+          
+        for(int i=0 ; i<=n ; i++){
+
+            int j=i;
+
+            int cnt = 0;
+
+            while(j>0){
+
+               if((j&1) == 1){
+                cnt++;
+               }
+
+                j = j>>1;
+
+            }
+            ans.push_back(cnt);
+        }
+
+        return ans;
+    }
+};
